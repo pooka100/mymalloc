10204a7160774e122c7ce43050ae6969ddfcbf07
chikisan1 <chikaomaeda1gmail.com>
2017-02-26 14:17:51 -0500

    finished memgrind

--- memgrind.c
+++ memgrind.c
@@ -3,13 +3,13 @@
 #include <time.h>
 #include "mymalloc.h"
 
-
-extern static char* myblock;
+void* Malloc(size_t size);
+void Free(void* ptr);
 
 void testA(){
 	char* ptrs[1000];
 	for(int i = 0; i < 1000; i++){
-		ptrs[i] = *Malloc(sizeof(char));
+		ptrs[i] = (char*)Malloc(sizeof(char));
 	}
 	for(int i = 0; i < 1000; i++){
 		Free(ptrs[i]);
@@ -20,7 +20,7 @@ void testA(){
 void testB(){
 	char* ptrs[1000];
 	for(int i = 0; i < 1000; i++){
-		ptrs[i] = *Malloc(sizeof(char));
+		ptrs[i] = (char*)Malloc(sizeof(char));
 		Free(ptrs[i]);
 	}
 }
@@ -29,14 +29,17 @@ void testC(){
 	char* ptrs[1000];
 	int m = 0;
 	int f = 0;
+	for(int i = 0; i < 1000; i++){
+		ptrs[i] = NULL;
+	}
 	while(f < 1000){
 		int random = rand() % 2; 
-		if(random == 0){
-			ptrs[m] = *Malloc(sizeof(char));
+		if(random == 0 && m < 1000){
+			ptrs[m] = (char*)Malloc(sizeof(char));
 			m++;
 		}
 		else{
-			if(ptrs[f] != null){
+			if(ptrs[f] != NULL){
 				Free(ptrs[f]);
 				f++;
 			}
@@ -46,37 +49,116 @@ void testC(){
 
 void testD(){
 	char* ptrs[1000];
-	int m = 0;
-	int f = 0;
-	int max = 0
+	int m = 0; //count malloc
+	int f = 0; //count free
 	/*
 	 Randomly choose between a randomly-sized malloc() or free()ing a pointer – do this many
  	times (see below)
  	 Keep track of each malloc so that all mallocs do not exceed your total memory capacity
  	*/
-	while(f < 1000 && max < 5000){
+ 	for(int i = 0; i < 1000; i++){
+		ptrs[i] = NULL;
+	}
+	while(f < 1000){
 		int random = rand() % 2; 
 		if(random == 0 && m < 1000){
 			//Choose a random allocation size between 1 and 64 bytes
 			int rSize = rand() % 64 + 1;
 			size_t size = (size_t) rSize;
 			//- Keep track of each operation so that you eventually malloc() 1000 times
-			ptrs[m] = *Malloc(size);
-			max += rSize;
-			m++;
+			char* ptr = (char*)Malloc(size);
+			if(ptr != NULL){
+				ptrs[m] = (char*)Malloc(size);
+				m++;
+			}
 		}
 		else{
 			//- Keep track of each operation so that you eventually free() all pointers
-			if(ptrs[f] != null){
+			if(ptrs[f] != NULL){
 				Free(ptrs[f]);
 				f++;
 			}
 		}
 	}
-	
-	
 }
+//test with structs 
+//testing memory consistency 
+struct testcase{
+	int array[100]; 
+	char stuff;
+};
+void testE(){
+ 	struct testcase* ptrs[10];
+ 	for(int i = 0; i < 10; i++){
+ 		ptrs[i] = (struct testcase*)Malloc(sizeof(struct testcase));
+ 		for(int k = 0; k < 100; k++){
+ 			ptrs[i]->array[k] = i*10 + k;
+ 		}
+ 		ptrs[i]->stuff = i;
+ 	}
+ 	for(int i = 0; i < 10; i++){
+ 		for(int k = 0; k < 100; k++){
+ 			if(ptrs[i]->array[k] != i*10 + k){
+ 				printf("this is wrong\n");
+ 			}	
+ 		}
+ 		if(ptrs[i]->stuff != i){
+ 			printf("this is wrong\n");
+ 		}	
+ 		Free(ptrs[i]);
+ 	}
+
+}
+
+void testF(){
+	char* ptrs[1000];
+	for(int i = 0; i < 1000; i++){
+		ptrs[i] = (char*)Malloc(sizeof(char));
+	}
+	for(int i = 0; i < 1000; i++){
+		char* temp = ptrs[i];
+		Free(ptrs[i]);
+		ptrs[i] = (char*)Malloc(sizeof(char));
+		if(temp != ptrs[i]){
+			printf("Is not consistent with first free algorithm.\n");
+		}
+	}
+	for(int i = 0; i < 1000; i++){
+		Free(ptrs[i]);
+	}
+
+}
+
+double runtime(void (*test)(void)){
+	clock_t start = clock();
+	for(int i = 0; i < 100; i++){
+		test();
+	}
+	clock_t end = clock();
+	return (double)(end - start) / CLOCKS_PER_SEC / 100.0;
+}
+	
 int main(int arg, char* argv[]){
+
+	printf("run testA\n");
+	double timeA = runtime(testA);
+	printf("Mean time A, %f\n", timeA);
+	printf("run testB\n");
+	double timeB = runtime(testB);
+	printf("Mean time B, %f\n", timeB);
+	printf("run testC\n");
+	double timeC = runtime(testC);
+	printf("Mean time C, %f\n", timeC);
+	printf("run testD\n");
+	double timeD = runtime(testD);
+	printf("Mean time D, %f\n", timeD);
+	printf("run testE\n");
+	double timeE = runtime(testE);
+	printf("Mean time E, %f\n", timeE);
+	printf("run testF\n");
+	double timeF = runtime(testF);
+	printf("Mean time F, %f\n", timeF);
+
 
 
 }
